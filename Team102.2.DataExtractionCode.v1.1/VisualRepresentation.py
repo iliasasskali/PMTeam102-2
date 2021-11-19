d029289f1bf57ec35b83a0e4f0745001c7217148
Elias Asskali <elias.asskali01@estudiant.upf.edu>
2021-11-19 17:31:38 +0100

    Data

--- Team102.2.DataExtractionCode.v1.1/VisualRepresentation.py
+++ Team102.2.DataExtractionCode.v1.1/VisualRepresentation.py
@@ -13,14 +13,17 @@ def load_data():
     df_demografia = pd.read_excel(xls, 'Demografia', index_col=0)
     df_lloguer = pd.read_excel(xls, 'Lloguer-Decada',  index_col=1)
     df_edat_sexe = pd.read_excel(xls, 'Poblacio-Edat-Sexe', index_col=2)
-    #df_demografi_habitatges = pd.read_excel(xls, 'Demografia-habitatges')
+    df_demografia_habitatges = pd.read_excel(xls, 'Demografia-Habitatges', index_col=0)
 
-    return df_poblacio, df_demografia, df_lloguer, df_edat_sexe
-
+    return df_poblacio, df_demografia, df_lloguer, df_edat_sexe, df_demografia_habitatges
 
 st.title('Tarragonès Data')
+df_poblacio, df_demografia, df_lloguer, df_edat_sexe, df_demografia_habitatges = load_data()
 
-df_poblacio, df_demografia, df_lloguer, df_edat_sexe = load_data()
+multiselect = st.sidebar.multiselect(
+    "Filter info by municipi or select multiple municipis to compare them:",
+    df_poblacio.index
+)
 
 st.subheader('Poblation Data')
 st.write(df_poblacio)
@@ -33,3 +36,14 @@ st.write(df_lloguer)
 
 st.subheader('Age Gender Data')
 st.write(df_edat_sexe)
+
+st.subheader('Age Gender Data')
+st.write(df_demografia_habitatges)
+
+st.bar_chart(df_lloguer["renda"])
+
+if st.checkbox('Show Age Gender Data'):
+    st.subheader('Age Gender Data')
+    st.write(df_edat_sexe)
+
+st.line_chart(df_demografia)
